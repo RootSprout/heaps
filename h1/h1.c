4bc47b7cf4178809b765c2e951abf93487365d4d
Meerthika <meerthikasuniverse@gmail.com>
2025-09-30 10:15:17 +0000

    [Change] pointers and free in h1

--- h1/h1.c
+++ h1/h1.c
@@ -3,9 +3,26 @@
 
 int main(){
     int *a = malloc(32);
-    int *b = malloc(64);
+    int *b = malloc(32);
+    int *c = malloc(32);
+
+    printf("Allocated a: %p\n b: %p\n c: %p\n", a,b,c);
+
     free(a);
-    int *c = malloc(128);
+    free(b);
+    free(c);
+
+    printf("Freed a, b, c\n");
+
+    printf("Freed a, b, c\n");
+
+    //Allocate again (tcache will reuse freed chunks)
+    char *d = malloc(0x20); //32 bytes
+    char *e = malloc(0x20);
+
+    printf("Re-allocated:\n d = %p\n e = %p\n", d, e);
+
     return 0;
+
 }
 
